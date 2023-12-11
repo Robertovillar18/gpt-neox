11de0cd0b4533ba421b1174e6805e765e95e6db2
roberto.villar <youremail@email.com>
2023-12-11 01:20:14 -0300

    test exec

--- train_model.py
+++ train_model.py
@@ -13,80 +13,80 @@ bnb_config = BitsAndBytesConfig(
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=bnb_config, device_map={"":0})
 
-# %%
-from peft import prepare_model_for_kbit_training
+# # %%
+# from peft import prepare_model_for_kbit_training
 
-model.gradient_checkpointing_enable()
-model = prepare_model_for_kbit_training(model)
+# model.gradient_checkpointing_enable()
+# model = prepare_model_for_kbit_training(model)
 
-# %%
-def print_trainable_parameters(model):
-    """
-    Prints the number of trainable parameters in the model.
-    """
-    trainable_params = 0
-    all_param = 0
-    for _, param in model.named_parameters():
-        all_param += param.numel()
-        if param.requires_grad:
-            trainable_params += param.numel()
-    print(
-        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
-    )
+# # %%
+# def print_trainable_parameters(model):
+#     """
+#     Prints the number of trainable parameters in the model.
+#     """
+#     trainable_params = 0
+#     all_param = 0
+#     for _, param in model.named_parameters():
+#         all_param += param.numel()
+#         if param.requires_grad:
+#             trainable_params += param.numel()
+#     print(
+#         f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+#     )
 
-# %%
-from peft import LoraConfig, get_peft_model
+# # %%
+# from peft import LoraConfig, get_peft_model
 
-config = LoraConfig(
-    r=8, 
-    lora_alpha=32, 
-    target_modules=["query_key_value"], 
-    lora_dropout=0.05, 
-    bias="none", 
-    task_type="CAUSAL_LM"
-)
+# config = LoraConfig(
+#     r=8, 
+#     lora_alpha=32, 
+#     target_modules=["query_key_value"], 
+#     lora_dropout=0.05, 
+#     bias="none", 
+#     task_type="CAUSAL_LM"
+# )
 
-model = get_peft_model(model, config)
-print_trainable_parameters(model)
+# model = get_peft_model(model, config)
+# print_trainable_parameters(model)
 
-# %%
-from datasets import load_dataset
+# # %%
+# from datasets import load_dataset
 
-data = load_dataset("Abirate/english_quotes")
-data = data.map(lambda samples: tokenizer(samples["quote"]), batched=True)
+# data = load_dataset("Abirate/english_quotes")
+# data = data.map(lambda samples: tokenizer(samples["quote"]), batched=True)
 
-# %%
-import transformers
+# # %%
+# import transformers
 
-# needed for gpt-neo-x tokenizer
-tokenizer.pad_token = tokenizer.eos_token
+# # needed for gpt-neo-x tokenizer
+# tokenizer.pad_token = tokenizer.eos_token
 
-trainer = transformers.Trainer(
-    model=model,
-    train_dataset=data["train"],
-    args=transformers.TrainingArguments(
-        per_device_train_batch_size=1,
-        gradient_accumulation_steps=4,
-        warmup_steps=2,
-        max_steps=10,
-        learning_rate=2e-4,
-        fp16=True,
-        logging_steps=1,
-        output_dir="outputs",
-        optim="paged_adamw_8bit"
-    ),
-    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
-)
-model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
-trainer.train()
+# trainer = transformers.Trainer(
+#     model=model,
+#     train_dataset=data["train"],
+#     args=transformers.TrainingArguments(
+#         per_device_train_batch_size=1,
+#         gradient_accumulation_steps=4,
+#         warmup_steps=2,
+#         max_steps=10,
+#         learning_rate=2e-4,
+#         fp16=True,
+#         logging_steps=1,
+#         output_dir="outputs",
+#         optim="paged_adamw_8bit"
+#     ),
+#     data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
+# )
+# model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
+# trainer.train()
 
-# %%
-model_to_save = trainer.model.module if hasattr(trainer.model, 'module') else trainer.model  # Take care of distributed/parallel training
-model_to_save.save_pretrained("outputs")
+# # %%
+# model_to_save = trainer.model.module if hasattr(trainer.model, 'module') else trainer.model  # Take care of distributed/parallel training
+# model_to_save.save_pretrained("outputs")
 
-# %%
-lora_config = LoraConfig.from_pretrained('outputs')
-model = get_peft_model(model, lora_config)
+# # %%
+# lora_config = LoraConfig.from_pretrained('outputs')
+# model = get_peft_model(model, lora_config)
 
 # %%
 text = "Elon Musk "
